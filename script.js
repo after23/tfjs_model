5d9b67a1f8edb2065200234db727ee9aac36690b
Afif <afiftrifarhan@gmail.com>
2021-04-13 13:39:09 +0700

    test

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const ans = document.getElementById("answer");
 
 async function loadModel() {
     model = undefined;
-    model = await tf.loadLayersModel("https://raw.githubusercontent.com/after23/tfjs_model/main/model.json");
+    model = await tf.loadLayersModel("https://raw.githubusercontent.com/after23/tfjs_model/main/noiseless-augmented-4-apr/1FC-1LSTM/model.json");
     console.log("model loaded")
   }
   loadModel();
 
   function make_prediction() {
     var a, b, c, d, e, f, g, output;
-    a = (Number(document.getElementById("first").value)+90)/90;
-    b = (Number(document.getElementById("second").value)+90)/90;
-    c = (Number(document.getElementById("third").value)+90)/90;
-    d = (Number(document.getElementById("fourth").value)+90)/90;
-    e = (Number(document.getElementById("fifth").value)+90)/90;
-    f = (Number(document.getElementById("sixth").value)+90)/90;
-    g = (Number(document.getElementById("seventh").value)+90)/90;
-    var input = [a,b,c,d,e,f,g];
-    for (var x = 7; x < 70; x++){
+    a = (Number(document.getElementById("first").value)+100)/100;
+    b = (Number(document.getElementById("second").value)+100)/100;
+    c = (Number(document.getElementById("third").value)+100)/100;
+    d = (Number(document.getElementById("fourth").value)+100)/100;
+    //e = (Number(document.getElementById("fifth").value)+90)/90;
+    //f = (Number(document.getElementById("sixth").value)+90)/90;
+    //g = (Number(document.getElementById("seventh").value)+90)/90;
+    var input = [a,b,c,d];
+    for (var x = 4; x < 40; x++){
         input.push(Math.floor( Math.random() * 90 )/90)
     }
     // var test = []
@@ -33,7 +33,7 @@ async function loadModel() {
     // console.log(test)
 
     console.log(input)
-    input_xs = tf.tensor3d(input, [1,10,7]);
+    input_xs = tf.tensor3d(input, [1,10,4]);
     output = model.predict(input_xs);
     console.log(output)
     const outputData = output.dataSync();
