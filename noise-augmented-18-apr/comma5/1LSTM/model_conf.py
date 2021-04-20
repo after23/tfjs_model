3e9c84fdeb142dc079abf18d074e6ac605c76299
Afif <afiftrifarhan@gmail.com>
2021-04-20 15:59:11 +0700

    model 1 LSTM

---
+++ noise-augmented-18-apr/comma5/1LSTM/model_conf.py
@@ -0,0 +1,14 @@
+ tf.keras.backend.clear_session()
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(10)),
+  tf.keras.layers.Dense(2),
+
+])
+
+
+optimizer = tf.keras.optimizers.SGD(lr=1e-1, momentum=0.9)
+model.compile(loss=tf.keras.losses.Huber(),
+              optimizer=optimizer,
+              metrics=["mse"])
+history = model.fit(np.array(x_train), np.array(y_train), batch_size=100, validation_split=0.1 , epochs=150)
