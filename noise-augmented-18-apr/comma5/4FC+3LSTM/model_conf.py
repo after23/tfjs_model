7b91618adcad2b5afedae98424996a4112cdc67b
Afif <afiftrifarhan@gmail.com>
2021-04-20 15:22:32 +0700

    model 4FC+3LSTM

---
+++ noise-augmented-18-apr/comma5/4FC+3LSTM/model_conf.py
@@ -0,0 +1,22 @@
+ tf.keras.backend.clear_session()
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Dense(60, activation="relu"),
+  tf.keras.layers.Dense(50, activation="relu"),
+  tf.keras.layers.Dropout(.2),
+  tf.keras.layers.Dense(40, activation="relu"),
+  tf.keras.layers.Dense(30, activation="relu"),
+  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(30, return_sequences=True)),
+  tf.keras.layers.Dropout(.2),
+  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(20, return_sequences=True)),
+  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(10)),
+  tf.keras.layers.Dense(2),
+
+])
+
+
+optimizer = tf.keras.optimizers.SGD(lr=1e-1, momentum=0.9)
+model.compile(loss=tf.keras.losses.Huber(),
+              optimizer=optimizer,
+              metrics=["mse"])
+history = model.fit(np.array(x_train), np.array(y_train), batch_size=100, validation_split=0.1 , epochs=150)
