838eebedc041c5ce529ce6c8d906dc39012591c5
Afif <afiftrifarhan@gmail.com>
2021-04-21 14:52:07 +0700

    model 2FC

---
+++ noise-augmented-18-apr/FC only/2FC/model_conf.py
@@ -0,0 +1,15 @@
+tf.keras.backend.clear_session()
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Dense(20, activation="relu"),
+  tf.keras.layers.Dense(10, activation="relu"),
+  tf.keras.layers.Dense(2),
+
+])
+
+
+optimizer = tf.keras.optimizers.SGD(lr=1e-3, momentum=0.9)
+model.compile(loss=tf.keras.losses.Huber(),
+              optimizer=optimizer,
+              metrics=["mse"])
+history = model.fit(np.array(x_train), np.array(y_train), validation_split=0.1, epochs=150)
