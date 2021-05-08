8ca1afe3f6873eb311f9feb17b85bf3bc26f89c9
Afif <afiftrifarhan@gmail.com>
2021-05-08 21:17:45 +0700

    model 2FC+1LSTM

---
+++ noise-augmented-18-apr/new_conf/2FC+1LSTM/model_conf.py
@@ -0,0 +1,17 @@
+ tf.keras.backend.clear_session()
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Dense(64, activation="relu"),
+  tf.keras.layers.Dense(32, activation="relu"),
+  tf.keras.layers.Dropout(.3),
+  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+  tf.keras.layers.Dense(2),
+
+])
+
+
+optimizer = tf.keras.optimizers.Adagrad(lr=1e-1)
+model.compile(loss=tf.keras.losses.Huber(),
+              optimizer=optimizer,
+              metrics=["mse"])
+history = model.fit(np.array(x_train), np.array(y_train), batch_size=128, validation_split=0.1 , epochs=150)
