818f90a6b40fcf5c96fd2708692e6764aa14dee8
Afif <afiftrifarhan@gmail.com>
2021-05-08 17:34:28 +0700

    new conf

---
+++ noise-augmented-18-apr/new_conf/5FC+2LSTM/model_conf.py
@@ -0,0 +1,23 @@
+ tf.keras.backend.clear_session()
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Dense(60, activation="relu"),
+  tf.keras.layers.Dense(50, activation="relu"),
+  tf.keras.layers.Dropout(.3),
+  tf.keras.layers.Dense(40, activation="relu"),
+  tf.keras.layers.Dense(30, activation="relu"),
+  tf.keras.layers.Dropout(.3),
+  tf.keras.layers.Dense(20, activation="relu"),
+  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
+  tf.keras.layers.Dropout(.3),
+  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+  tf.keras.layers.Dense(2),
+
+])
+
+
+optimizer = tf.keras.optimizers.Adagrad(lr=1e-1)
+model.compile(loss=tf.keras.losses.Huber(),
+              optimizer=optimizer,
+              metrics=["mse"])
+history = model.fit(np.array(x_train), np.array(y_train), batch_size=100, validation_split=0.1 , epochs=150)
---
+++ noise-augmented-18-apr/new_conf/evaluasi.md
@@ -0,0 +1,23 @@
+# Hasil evaluasi
+
+
+Model | Loss | MSE | Muter | zig-zag
+--- | --- | --- | --- | ---
+1FC+1LSTM |  |  |  | 
+2FC+1LSTM |  |  |  | 
+3FC+1LSTM |  |  |  | 
+4FC+1LSTM |  |  |  | 
+5FC+1LSTM |  |  |  | 
+1FC+2LSTM |  |  |  |
+2FC+2LSTM |  |  |  | 
+3FC+2LSTM |  |  |  | 
+4FC+2LSTM |  |  |  | 
+5FC+2LSTM | 0.028300879523158073 | 0.056650105863809586 | 0.48163673281669617 | 0.8973722457885742
+1FC+3LSTM |  |  |  | 
+2FC+3LSTM |  |  |  | 
+3FC+3LSTM |  |  |  | 
+4FC+3LSTM |  |  |  | 
+5FC+3LSTM |  |  |  | 
+1LSTM |  |  |  | 
+2LSTM |  |  |  | 
+3LSTM |  |  |  | 
